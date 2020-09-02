c9ce5fd55536262de63bf908f2345fae80d21d75
Michele Prova <michele.prova99@gmail.com>
2020-09-02 18:13:19 +0200

    completo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
 $(document).ready(function() {
-	//Code
+
+  $.ajax(
+
+    {
+
+      url: "https://flynn.boolean.careers/exercises/api/array/music",
+
+      method: "GET",
+
+      success: function(risposta){
+
+        insertCd(risposta);
+
+      },
+
+      error: function(){
+        alert("errore");
+      }
+
+    }
+
+  );
+
+  $("#scelta option").click(function(){
+
+    var genere = $(this).html();
+
+    if (genere == "All") {
+
+      $(".cd").show();
+
+    } else {
+
+      $(".cd").hide();
+
+      $(".cd." + genere).show();
+
+    }
+
+  });
+
 });
+
+function insertCd(data) {
+
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+
+  for (var i = 0; i < data.response.length; i++) {
+
+    var context = data.response[i];
+    var html = template(context);
+
+    $(".cds-container").append(html);
+
+  }
+
+}
